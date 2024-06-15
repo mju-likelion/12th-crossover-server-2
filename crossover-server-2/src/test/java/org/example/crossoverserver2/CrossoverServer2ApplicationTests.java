585869a2c0a3f65f022c7bf04dae2d41938d9860
jher235 <tim668666@gmail.com>
2024-06-15 17:38:03 +0900

    setting:Spring Initializer init - #2

---
+++ crossover-server-2/src/test/java/org/example/crossoverserver2/CrossoverServer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crossoverserver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossoverServer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
