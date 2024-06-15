0f6db23ace96153247a5e95cd6f3eae2774fc771
jher235 <tim668666@gmail.com>
2024-06-15 18:42:05 +0900

    feature:ERD_생성-#3

---
+++ crossover-server-2/src/main/java/model/Comment.java
@@ -0,0 +1,28 @@
+package model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Comment  extends BaseEntity {
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private Board board;
+
+}
---
+++ crossover-server-2/src/main/java/model/User.java
@@ -0,0 +1,45 @@
+package model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "user")
+public class User extends BaseEntity {
+
+
+    @Column(nullable = false, unique = true)
+    private String userId;
+
+    @Setter
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Setter
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Setter
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true , fetch = FetchType.LAZY)
+    private List<Board> boards;
+
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Comment> comments;
+
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<UserClause> userClause;
+
+
+}
