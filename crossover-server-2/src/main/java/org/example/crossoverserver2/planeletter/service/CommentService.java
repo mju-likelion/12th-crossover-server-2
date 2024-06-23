9f99b47fc369ecb7a585b077886128c6601e6b41
jher235 <tim668666@gmail.com>
2024-06-23 18:50:04 +0900

    refactor: Board, Comment Service 리팩토링 - #51 - Board와 Comment의 비즈니스 로직 부분을 리팩토링하고 주석으로 설명을 달아두었습니다. - 주석의 경우 읽으신 후 삭제해서 다시 pr을 올려주세요

--- crossover-server-2/src/main/java/org/example/crossoverserver2/planeletter/service/CommentService.java
+++ crossover-server-2/src/main/java/org/example/crossoverserver2/planeletter/service/CommentService.java
@@ -8,6 +8,7 @@ import org.example.crossoverserver2.planeletter.dto.response.comment.CommentList
 import org.example.crossoverserver2.planeletter.exception.ForbiddenException;
 import org.example.crossoverserver2.planeletter.exception.NotFoundException;
 import org.example.crossoverserver2.planeletter.exception.errorcode.ErrorCode;
+import org.example.crossoverserver2.planeletter.model.Board;
 import org.example.crossoverserver2.planeletter.model.Comment;
 import org.example.crossoverserver2.planeletter.model.User;
 import org.example.crossoverserver2.planeletter.repository.BoardRepository;
@@ -22,12 +23,15 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+
 @Service
 @AllArgsConstructor
 public class CommentService {
 
     public final CommentRepository commentRepository;
     public final BoardRepository boardRepository;
+
+    private final static int COMMENT_PAGE_SIZE = 10;
 
     //댓글 작성
     public void writeComment(User user, UUID boardId, WriteCommentDto writeCommentDto){
@@ -44,7 +48,7 @@ public class CommentService {
     public CommentListResponseData getCommentList(UUID boardId, int page){
         existsBoard(boardId);
 
-        int pageSize = 10;
+        int pageSize = COMMENT_PAGE_SIZE;
         Sort sort = Sort.by(Sort.Order.desc("createdAt"));  //작성순 정렬
         Pageable pageable = PageRequest.of(page, pageSize, sort);   //페이지의 번호, 사이즈, 정렬 조건 설정
 
@@ -78,21 +82,38 @@ public class CommentService {
     //댓글 삭제
     public void removeCommentById(User user, UUID boardId, UUID id){
         existsBoard(boardId);
-        checkUser(user, id);
+        Comment comment = findCommentById(id);
+        checkUser(user, comment);
         commentRepository.deleteById(id);
     }
 
     //게시글 존재 여부 확인
-    public boolean existsBoard(UUID boardId){
-        if(boardRepository.existsById(boardId)){
-            return true;
-        } throw new NotFoundException(ErrorCode.NOT_FOUND_BOARD);
+    //사실 이 코드는 굳이? 싶을 수도 있어서 기존 코드를 사용하셔도 괜찮은데, 기본적으로 return true를 반환할 필요가 없기 때문에 void로 작성하시는 게 보기에 조금 더 좋을 수 있을 것 같아서 건드려 봤습니다.
+//    public boolean existsBoard(UUID boardId){
+//        if(boardRepository.existsById(boardId)){
+//            return true;
+//        } throw new NotFoundException(ErrorCode.NOT_FOUND_BOARD);
+//    }
+    private void existsBoard(UUID boardId){
+        if (!boardRepository.existsById(boardId)){
+            throw new NotFoundException(ErrorCode.NOT_FOUND_BOARD);
+        }
+    }
+
+    private Comment findCommentById(UUID commentId){
+        return commentRepository.findById(commentId).orElseThrow(()-> new NotFoundException(ErrorCode.NOT_FOUND_COMMENT));
     }
 
     //해당 접근 유저가 댓글 작성자와 일치하는지 확인
-    public boolean checkUser(User user, UUID id){
-        if(commentRepository.existsByUserAndId(user, id)){
-            return true;
-        } throw new ForbiddenException(ErrorCode.NO_ACCESS, "해당 댓글 접근 권한이 없습니다.");
+    //작성자를 확인하는 코드의 경우에 파라미터로 유저와 comment를 직접 받는 게 이후에 코드가 달라지더라도 재사용하기 좋다고 대현님이 말씀해주셔서 이렇게 바꿔봤습니다.
+//    public boolean checkUser(User user, UUID id){
+//        if(commentRepository.existsByUserAndId(user, id)){
+//            return true;
+//        } throw new ForbiddenException(ErrorCode.NO_ACCESS, "해당 댓글 접근 권한이 없습니다.");
+//    }
+    private void checkUser(User user, Comment comment){
+        if(!comment.getUser().getId().equals(user.getId())){
+            throw new ForbiddenException(ErrorCode.NO_ACCESS, "해당 댓글 접근 권한이 없습니다.");
+        }
     }
 }
